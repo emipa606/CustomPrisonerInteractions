82fc1eb7dd489c3ac041ce6362ea0624398376bd
Mlie <emipa606@users.noreply.github.com>
2022-08-08 20:16:05 +0200

    Should now set the correct followup action from successful conversion-rituals as well

---
+++ Source/CustomPrisonerInteractions/Pawn_IdeoTracker_IdeoConversionAttempt - Copy.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CustomPrisonerInteractions;
+
+[HarmonyPatch(typeof(RitualOutcomeEffectWorker_Conversion), "Apply")]
+public static class RitualOutcomeEffectWorker_Conversion_Apply
+{
+    public static void Prefix(LordJob_Ritual jobRitual, out Ideo __state)
+    {
+        var pawn = jobRitual.PawnWithRole("convertee");
+        __state = pawn.ideo.Ideo;
+    }
+
+    public static void Postfix(LordJob_Ritual jobRitual, Ideo __state)
+    {
+        var pawn = jobRitual.PawnWithRole("convertee");
+
+        if (!pawn.IsPrisonerOfColony)
+        {
+            return;
+        }
+
+        if (__state == pawn.ideo.Ideo)
+        {
+            return;
+        }
+
+        var extraInteractionsTracker = pawn.Map.GetExtraInteractionsTracker();
+        if (extraInteractionsTracker == null || !extraInteractionsTracker.Has(pawn))
+        {
+            return;
+        }
+
+        TaggedString text;
+
+        switch (extraInteractionsTracker[pawn])
+        {
+            case CustomPrisonerInteractions.ExtraMode.Release:
+            case CustomPrisonerInteractions.ExtraMode.ReduceResistanceThenRelease:
+                pawn.guest.interactionMode = PrisonerInteractionModeDefOf.Release;
+                extraInteractionsTracker[pawn] = CustomPrisonerInteractionsMod.instance.Settings.DefaultReleaseValue;
+                "CTRe.pawnconvertedrelease".Translate(pawn.NameFullColored);
+                break;
+            case CustomPrisonerInteractions.ExtraMode.ReduceResistanceThenRecruit:
+            case CustomPrisonerInteractions.ExtraMode.Recruit:
+                pawn.guest.interactionMode = PrisonerInteractionModeDefOf.AttemptRecruit;
+                "CTRe.pawnconvertedrecruit".Translate(pawn.NameFullColored);
+                break;
+            case CustomPrisonerInteractions.ExtraMode.Enslave:
+            case CustomPrisonerInteractions.ExtraMode.ReduceWillThenEnslave:
+                pawn.guest.interactionMode = PrisonerInteractionModeDefOf.Enslave;
+                "CTRe.pawnconvertedenslave".Translate(pawn.NameFullColored);
+                break;
+        }
+
+        var messageType = MessageTypeDefOf.PositiveEvent;
+        var message = new Message(text, messageType, new LookTargets(pawn));
+        Messages.Message(message);
+    }
+}
